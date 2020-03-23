4078e25df5159ddb90f1494fe1dbbbc20df1ab3b
retn0 <nbsp1221@gmail.com>
2020-03-23 12:15:22 +0900

    Fix not to apply redux-devtools-extension in production mode

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,9 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './modules';
 
+const DEBUG = process.env.NODE_ENV === 'development';
+
 export default createStore(
 	rootReducer,
-	composeWithDevTools()
+	DEBUG && composeWithDevTools()
 );
